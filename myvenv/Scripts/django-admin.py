16afc5f51f35899616ae057ba7bd2e14f9959a93
Raj <raj.kalsariya1994@gmail.com>
2020-12-29 15:24:31 +0530

    created Django project and app

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\innovate369\djangoproject\profile-rest-api\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
